aa880efae70c9b3684f42fabe258499ab3202651
unknown <e0035221@u.nus.edu>
2018-03-21 00:13:03 +0800

    stuff

--- main.c
+++ main.c
@@ -5,19 +5,29 @@
 #include "lpc17xx_i2c.h"
 #include "lpc17xx_spi.h"
 #include "LPC17xx.h"
+#include "core_cm3.h"
 
+#include "oled.h"
 #include "rgb.h"
 #include "temp.h"
 #include "led7seg.h"
 
 volatile uint32_t msTicks;
+volatile uint8_t mode;
 
-void SysTick_Handler(void){
-	msTicks++;
-}
+#define PRESCALE (25000-1)
 
-uint32_t getTicks(void){
-	return msTicks;
+void init_Timer0(void)
+{
+	/*Assuming that PLL0 has been setup with CCLK = 100Mhz and PCLK = 25Mhz.*/
+	LPC_SC->PCONP |= (1<<1); //Power up TIM0. By default TIM0 and TIM1 are enabled.
+	LPC_SC->PCLKSEL0 &= ~(0x3<<3); //Set PCLK for timer = CCLK/4 = 100/4 (default)
+
+	LPC_TIM0->CTCR = 0x0;
+	LPC_TIM0->PR = PRESCALE; //Increment TC at every 24999+1 clock cycles
+	//25000 clock cycles @25Mhz = 1 mS
+
+	LPC_TIM0->TCR = 0x02; //Reset Timer
 }
 
 static void init_GPIO(void)
@@ -33,14 +43,17 @@ static void init_GPIO(void)
 	PINSEL_ConfigPin(&PinCfg);
 	GPIO_SetDir(2, 1<<10, 0);
 
-	PINSEL_CFG_Type PinCfg2;
-	PinCfg2.Funcnum = 0;
-	PinCfg2.OpenDrain = 0;
-	PinCfg2.Pinmode = 0;
-	PinCfg2.Portnum = 1;
+	PinCfg.Funcnum = 0;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 1;
 	PinCfg.Pinnum = 31;
-	PINSEL_ConfigPin(&PinCfg2);
+	PINSEL_ConfigPin(&PinCfg);
 	GPIO_SetDir(1, 1<<31, 0);
+
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 6;
+	PINSEL_ConfigPin(&PinCfg);
 }
 
 static void init_ssp(void)
@@ -99,43 +112,95 @@ static void init_i2c(void)
 	I2C_Cmd(LPC_I2C2, ENABLE);
 }
 
+void SysTick_Handler(void){
+	msTicks++;
+}
+
+void EINT3_IRQHandler(void){
+	if((LPC_GPIOINT->IO2IntStatR>>10) & 0x1){
+		//print statements in interrupts cause a lot of overhead, use flags instead to print statements
+		printf("Button right pressed.\n");
+		mode = 0x01;
+		//clears the interrupt (have to do this or the interrupt will loop infinitely
+		LPC_GPIOINT->IO2IntClr = 1<<10;
+	}
+}
+
+void delayMS(unsigned int milliseconds) //Using Timer0
+{
+	LPC_TIM0->TCR = 0x02; //Reset Timer
+
+	LPC_TIM0->TCR = 0x01; //Enable timer
+
+	while(LPC_TIM0->TC < milliseconds); //wait until timer counter reaches the desired delay
+
+	LPC_TIM0->TCR = 0x00; //Disable timer
+}
+
+uint32_t getTicks(void){
+	return msTicks;
+}
+
 int main (void) {
 
-
-
 	SysTick_Config(SystemCoreClock/1000);
-	uint32_t my_temp_value;
-
-    uint8_t btn1 = 1;
-    uint8_t btn2 = 1;
 
     init_GPIO();
-    rgb_init();
     init_i2c();
     init_ssp();
+	init_Timer0();
+
+    rgb_init();
+    oled_init();
     led7seg_init();
 	temp_init(getTicks);
 
+	LPC_GPIOINT->IO2IntEnR |= 1<<10;
+	NVIC_EnableIRQ(EINT3_IRQn);
+
+	mode = 0; //init as STATIONARY MODE
+	char* stationaryStrPtr;
+	char* stationaryTempPtr[50]={};
+	uint32_t temp_value;
+	uint32_t stationary_counter = 15;
+	uint8_t sseg_chars[] = {
+	        /* digits 0 - 9 */
+	        0x24, 0x7D, 0xE0, 0x70, 0x39, 0x32, 0x22, 0x7C, 0x20, 0x38,
+	        /* A to F */
+	        0x28, 0x23, 0xA6, 0x61, 0xA2, 0xAA
+	};
+
+	//stationaryTempPtr = "Temp:";
+	stationaryStrPtr = "STATIONARY";
+	oled_clearScreen(OLED_COLOR_BLACK);
+	oled_putString(20, 20, (unsigned char*)stationaryStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+	oled_putString(20, 28, (unsigned char*)stationaryTempPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+	led7seg_setChar(sseg_chars[stationary_counter], TRUE);
+
     while (1)
     {
+    	/* <---STATIONARY MODE---> */
+    	if(mode == 0x00){
+    		temp_value = temp_read();
+    		sprintf(stationaryTempPtr, "Temp: %2.2f", temp_value/10.0);
+    		oled_putString(20, 28, (unsigned char*)stationaryTempPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    		printf("checking temp \n");
+    	}
+    	/* <---STATIONARY TO LAUNCH---> */
+    	else if(mode == 0x01){
+    		if(stationary_counter == 0){
+    			mode = 0x02;
+    		} else {
+    			temp_value = temp_read();
+    			sprintf(stationaryTempPtr, "Temp: %2.2f", temp_value/10.0);
+    			oled_putString(20, 28, (unsigned char*)stationaryTempPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+    			printf("checking temp \n");
 
-    	my_temp_value = temp_read();
-    	printf("%2.2f degrees \n", my_temp_value/10.0);
-    	Timer0_Wait(1);
+    			stationary_counter = stationary_counter - 1;
+    			led7seg_setChar(sseg_chars[stationary_counter], TRUE);
 
-
-    	btn1 = (GPIO_ReadValue(2) >> 10) & 0x01;
-    	btn2 = (GPIO_ReadValue(1) >> 31) & 0x01;
-        if (btn1 == 0 && btn2==0)
-        {
-        	GPIO_ClearValue(2,1);
-            GPIO_SetValue(0,1<<26);
-
-            led7seg_setChar(0x24, TRUE);
-        } else {
-        	GPIO_ClearValue(0, (1<<26));
-        	GPIO_SetValue(2,1);
-            led7seg_setChar(0xFF, TRUE);
-        }
+    	    	delayMS(1000);
+    		}
+    	}
     }
 }
