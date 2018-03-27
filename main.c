ce92b9002e3db08f384a3c85b533e4de6529404f
unknown <e0035221@u.nus.edu>
2018-03-28 02:09:08 +0800

    temp+acc warnings, launch to return mode

--- main.c
+++ main.c
@@ -15,13 +15,18 @@
 #include "pca9532.h"
 
 volatile uint32_t msTicks;
-volatile uint32_t recordTicks;
 
+//MODE variables
 uint8_t mode;
+uint8_t mode_change_flag = 0;
 char* modeStrPtr;
 char* stateStrPtr;
-uint8_t toggle_count_flag = 0;
+int8_t toggle_count;
 uint8_t sw4btn;
+
+uint8_t blink_blue_flag = 0;
+uint8_t blink_red_flag = 0;
+uint8_t rgb_flag = 0;
 
 //seven segment variables
 uint32_t stationary_counter = 15;
@@ -53,7 +58,7 @@ char* accz[40];
 volatile uint32_t period;
 
 #define PRESCALE (25000-1)
-#define TEMP_HIGH_THRESHOLD 33.0
+#define TEMP_HIGH_THRESHOLD 31.0
 #define OBSTACLE_NEAR_THRESHOLD 3000
 #define ACC_THRESHOLD 0.4
 
@@ -71,23 +76,39 @@ uint32_t getTicks(void){
 }
 
 void TOGGLE_MODE(){
-	// if in STATIONARY mode, go to STATIONARY-TO-LAUNCH mode
+	// if in STATIONARY mode, go to COUNTDOWN mode
 	if(mode == 0x00){
 		mode = 0x01;
+		toggle_count = 0;
+		//COUNTDOWN has same oled display as STATIONARY, no need to enable mode_change_flag
 	}
 
 	// if in LAUNCH mode, check number of TOGGLE_MODE(SW3) button presses
-	else if(mode == 0x02 && toggle_count_flag==0){
-		toggle_count_flag = 0x01;
-	}
+	else if(mode == 0x02){
+		if(toggle_count >= 2){
+			mode = 0x03;
+			toggle_count = 0;
 
-	else if(mode == 0x02 && toggle_count_flag==0x01){
-		toggle_count_flag = 0x02;
+			//Clear all warnings
+			temp_warning_flag = 0;
+			acc_warning_flag = 0;
+			temp_warning_message_flag = 0;
+			acc_warning_message_flag = 0;
+			GPIO_ClearValue( 0, (1<<26) );
+			GPIO_ClearValue(2,1<<0);
+
+			//Change viewmode
+			mode_change_flag = 1;
+		}
 	}
 
 	// if in RETURN mode, go back to STATIONARY mode
 	else if(mode == 0x03){
 		mode = 0x00;
+		toggle_count = 0;
+
+		//Change viewmode
+		mode_change_flag = 1;
 	}
 
 	else {
@@ -99,28 +120,25 @@ void check_clearWarning(){
 	sw4btn = (GPIO_ReadValue(1) >> 31) & 0x01;
 	if(sw4btn == 0){
 		if(acc_warning_flag == 1){
+			//Turn off flag
 			acc_warning_flag = 0;
 			acc_warning_message_flag = 0;
+			//Clear rgb led and oled
+			blink_blue_flag = 0;
+		    GPIO_ClearValue( 0, (1<<26) );
 			oled_clearScreen(OLED_COLOR_BLACK);
 		}
 
 		if(temp_warning_flag == 1){
+			//Turn off flag
 			temp_warning_flag = 0;
 			temp_warning_message_flag = 0;
+			//Clear rgb led and oled
+			blink_red_flag = 0;
+			GPIO_ClearValue(2,1<<0);
 			oled_clearScreen(OLED_COLOR_BLACK);
 		}
-	} else {
-		return;
 	}
-}
-
-void blink_blue(){
-    GPIO_SetValue( 0, (1<<26) );
-    check_clearWarning();
-    delayMS(333);
-    GPIO_ClearValue( 0, (1<<26) );
-    check_clearWarning();
-    delayMS(333);
 }
 
 void ACCELEROMETER(){
@@ -133,21 +151,23 @@ void ACCELEROMETER(){
 	x=x+xoff;
 	y=y+yoff;
 
-	//need values in terms of g, according to acc.h, g level is set to default 2g
-	//divide value read by accelerometer by 64, according to datasheet
-	sprintf(accx, "X:%3.1f", x/64.0);
-	sprintf(accy, "Y:%3.1f", y/64.0);
-
-	//printf("X:%2.2f  Y:%2.2f \n",x/64.0,y/64.0); debugging printf, disable to improve performance
-
 	//check if accelerometer in g exceed threshold value
 	if(x/64.0 >= 0.4 || y/64.0 >= 0.4){
-		//printf("acc exceed\n"); //debugging printf, disable to improve performance
 		acc_warning_flag = 1;
-		oled_clearScreen(OLED_COLOR_BLACK);
+		if(acc_warning_flag == 0){
+			oled_clearScreen(OLED_COLOR_BLACK);
+		}
 	} else {
-		oled_putString(10, 38, (unsigned char*)accx, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-		oled_putString(55, 38, (unsigned char*)accy, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+		//need values in terms of g, according to acc.h, g level is set to default 2g
+		//divide value read by accelerometer by 64, according to datasheet
+
+		if(temp_warning_flag == 0 && acc_warning_flag == 0){ //make sure no warnings are triggered
+			sprintf(accx, "X:%3.1f", x/64.0);
+			sprintf(accy, "Y:%3.1f", y/64.0);
+
+			oled_putString(10, 38, (unsigned char*)accx, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+			oled_putString(55, 38, (unsigned char*)accy, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+		}
 	}
 }
 
@@ -170,7 +190,9 @@ void TEMP_SENSOR(){
 	//using TS0/TS1 configuration of GND/GND(both jumpers attached)
 	//Following datasheet formula:
 	//10temp_value(deg celcius) = 10(period(us)/scalar multiplier of 10) - 2731
-	temp_value = period/100 - 2731;
+	//if(mode_change_flag == 0){
+		temp_value = period/100 - 2731;
+	//}
 	if(temp_value/10.0 > TEMP_HIGH_THRESHOLD){
 		temp_warning_flag = 1;
 	}
@@ -181,7 +203,7 @@ static void init_GPIO(void)
 
 	// Initialize button SW4 and SW3
 	PINSEL_CFG_Type PinCfg;
-	PinCfg.Funcnum = 0;
+	PinCfg.Funcnum = 1;
 	PinCfg.OpenDrain = 0;
 	PinCfg.Pinmode = 0;
 	PinCfg.Portnum = 2;
@@ -269,10 +291,24 @@ void init_Timer0(void)
 	LPC_TIM0->TCR = 0x02; //Reset Timer
 }
 
+//Timer for 333ms intervals
+void init_Timer1(void){
+	LPC_SC->PCONP |= (1<<2);		//Turns on timer1 (On by default)
+	LPC_SC->PCLKSEL0 |= 1<<4;		//Set Timer3 CLK = CCLK(100MHZ)
+	LPC_TIM1->TCR = 0x02;			//Resets TC
+	LPC_TIM1->PR = 0x00;			//Prescale is 0
+	LPC_TIM1->MR3 = 33000000;		//Match Count 3(333ms)
+	LPC_TIM1->IR = 0xff;			//Reset Timer1 interrupts
+	LPC_TIM1->MCR |= (1<<10);		//Clears TC when TC hits MR3 value of 33000000
+	LPC_TIM1->MCR |= (1<<9);		//Triggers TIMER1 interrupt when TC hits MR3 value of 33000000
+	LPC_TIM1->TCR = 0x01;			//Start Timer1
+}
+
+//Timer for temperature sensor
 void init_Timer2(void){				//Initialization for timer2
 
 	LPC_SC->PCONP |= (1<<22); 		//Turns on timer2 (Off by default)
-	LPC_SC->PCLKSEL1 |= 1<< 12;		//Timer Clock = 100MHZ
+	LPC_SC->PCLKSEL1 |= 1<< 12;		//Set Timer2 CLK = CCLK(100MHZ)
 	LPC_TIM2->TCR = 0x02;			//Resets Timer Counter (TC)
 	LPC_TIM2->PR  = 0x00;			//Clock Prescaler = 0
 	//LPC_TIM2->MR0 = 33300000;		//Match Count 0 (1 Count = 10ns)
@@ -280,23 +316,95 @@ void init_Timer2(void){				//Initialization for timer2
 	//LPC_TIM2->MR2 = 99900000;		//Match Count 2
 	LPC_TIM2->MR3 = 100000000;		//Match Count 3
 	LPC_TIM2->IR  = 0xff;			//Resets Timer2 Interrupts
-	LPC_TIM2->MCR |= 1<<10;			//Clears TC when TC hits MR3 value of 100000000
+	LPC_TIM2->MCR |= (1<<10);		//Clears TC when TC hits MR3 value of 100000000 and triggers timer2 interrupt
+	LPC_TIM2->MCR |= (1<<9);
 	LPC_TIM2->TCR = 0x01;			//Start timer2
+}
 
-	//implement a timer interrupt for MR3 to reset number of button presses for transition from launch->return
+//Timer for 1 second intervals
+void init_Timer3(void){				//Initialization for timer3
+
+	LPC_SC->PCONP |= (1<<23); 		//Turns on timer3 (Off by default)
+	LPC_SC->PCLKSEL1 |= 1<< 14;		//Set Timer3 CLK = CCLK(100MHZ)
+	LPC_TIM3->TCR = 0x02;			//Resets Timer Counter (TC)
+	LPC_TIM3->PR  = 0x00;			//Clock Prescaler = 0
+	LPC_TIM3->MR3 = 100000000;		//Match Count 3 (1 second)
+	LPC_TIM3->IR  = 0xff;			//Resets Timer3 Interrupts
+	LPC_TIM3->MCR |= (1<<11);		//Stops timer when TC hits MR3 value of 100000000
+	LPC_TIM3->MCR |= (1<<10);		//Clears TC when TC hits MR3 value of 100000000
+	LPC_TIM3->MCR |= (1<<9);		//Triggers TIMER3 interrupt when TC hits MR3 value of 100000000
+	//LPC_TIM3->TCR = 0x01;			//Start timer3
 }
 
 void SysTick_Handler(void){
 	msTicks++;
 }
 
-void EINT3_IRQHandler(void){
-	if((LPC_GPIOINT->IO2IntStatR>>10) & 0x1){
-		TOGGLE_MODE();
-		LPC_GPIOINT->IO2IntClr = 1<<10;
+//333ms Timer interrupt
+void TIMER1_IRQHandler(void){
+	if(rgb_flag==4){
+		rgb_flag=0;
 	}
 
-	else if ((LPC_GPIOINT->IO0IntStatF)>>2 & 0x01){
+	if(acc_warning_flag == 1 && temp_warning_flag == 1){
+		GPIO_ClearValue(0, (1<<26));
+		GPIO_ClearValue(2, (1<<0));
+		if(rgb_flag==0){
+			GPIO_SetValue(0, 1<<26);
+			rgb_flag++;
+		}
+		else if(rgb_flag==1){
+			GPIO_ClearValue(0, 1<<26);
+			rgb_flag++;
+		}
+		else if(rgb_flag==2){
+			GPIO_SetValue(2,1<<0);
+			rgb_flag++;
+		}
+		else if(rgb_flag==3){
+			GPIO_ClearValue(2,1<<0);
+			rgb_flag++;
+		}
+	}
+
+	else if(acc_warning_flag == 1){
+		if(blink_blue_flag == 0){
+			GPIO_SetValue( 0, (1<<26) );
+			blink_blue_flag = 1;
+		} else {
+			GPIO_ClearValue( 0, (1<<26) );
+			blink_blue_flag = 0;
+		}
+	}
+
+	else if(temp_warning_flag == 1){
+		if(blink_red_flag == 0){
+			GPIO_SetValue( 2, (1<<0));
+			blink_red_flag = 1;
+		} else {
+			GPIO_ClearValue( 2, 1<<0 );
+			blink_red_flag = 0;
+		}
+	}
+	LPC_TIM1->IR |= 1<<0;
+}
+
+//1 second Timer Interrupt
+void TIMER3_IRQHandler(void){
+	toggle_count = 0;
+	LPC_TIM3->IR |= 1<<0;
+}
+
+void EINT0_IRQHandler(void){
+	//SW3 interrupt handler
+	toggle_count++;
+	TOGGLE_MODE();
+	LPC_TIM3->TCR = 0x01;
+	LPC_SC->EXTINT |= 1<<0;
+}
+
+void EINT3_IRQHandler(void){
+	if ((LPC_GPIOINT->IO0IntStatF)>>2 & 0x01){
 		TEMP_SENSOR();
 		LPC_GPIOINT->IO0IntClr = 1<<2;
 	}
@@ -307,36 +415,23 @@ void STATIONARY(){
 	modeStrPtr = "STATIONARY";
 	oled_putString(20, 20, (unsigned char*)modeStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 	oled_putString(20, 28, (unsigned char*)tempStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-	//HIGH TEMP CHECK
-	/*
-	if(temp_value/10.0 > TEMP_HIGH_THRESHOLD){
-		oled_clearScreen(OLED_COLOR_BLACK);
-	   	mode=0x05;
-	}else{
-	   	oled_putString(20, 28, (unsigned char*)tempStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-	}*/
 }
 
 void COUNTDOWN(){
 	//When countdown reaches 0, move to launch mode
 	if(stationary_counter == 0){
 		mode = 0x02; //enter LAUNCH mode
+		toggle_count = 0;
 		oled_clearScreen(OLED_COLOR_BLACK);
+
+		//reset timer 3 for launch mode
+		//LPC_TIM3->TCR = 0x02;
+		//LPC_TIM3->TCR = 0x01;
 	} else {
 		stationary_counter = stationary_counter - 1;
 		led7seg_setChar(sseg_chars[stationary_counter], TRUE);
 
 		sprintf(tempStrPtr, "Temp: %2.2f", temp_value/10.0);
-
-		//HIGH TEMP CHECK
-		if(temp_value/10.0 > TEMP_HIGH_THRESHOLD){
-		    oled_clearScreen(OLED_COLOR_BLACK);
-    		mode=0x05;
-    		stationary_counter = 15;
-    		led7seg_setChar(sseg_chars[stationary_counter], TRUE);
-		} else {
-			oled_putString(20, 28, (unsigned char*)tempStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-		}
     	delayMS(1000);
 	}
 }
@@ -347,38 +442,27 @@ void LAUNCH(){
 	oled_putString(20,20, (unsigned char*)modeStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 	oled_putString(20, 28, (unsigned char*)tempStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
 	ACCELEROMETER(); //read from accelerometer
-
-	//Interaction with TOGGLE_MODE(SW3) button in LAUNCH mode
-	if(getTicks()-recordTicks<1000){
-		//within 1 second
-		if(toggle_count_flag==0x02){
-			mode = 0x03; //go to return
-			acc_warning_flag = 0;
-			toggle_count_flag = 0;
-			oled_clearScreen(OLED_COLOR_BLACK);
-		}
-	} else {
-		//exceeded 1 second
-		if(toggle_count_flag==0x01){
-			mode = 0x00;
-			toggle_count_flag = 0;
-			oled_clearScreen(OLED_COLOR_BLACK);
-		}else{
-			recordTicks=getTicks();
-		}
-	}
 }
 
 void RETURN(){
 	modeStrPtr = "RETURN";
 	oled_putString(20,20, (unsigned char*)modeStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-	led7seg_setChar(0x24, TRUE); //set 0 on sseg
+	//led7seg_setChar(0xAA, TRUE); //set 0 on sseg
+}
+
+void CHANGE_VIEWMODE(){
+	if(mode_change_flag == 1){
+		oled_clearScreen(OLED_COLOR_BLACK);
+		mode_change_flag = 0;
+	}
 }
 
 void SET_MODE(){
 	if(acc_warning_flag == 1 || temp_warning_flag == 1){
 		return;
 	}
+
+	CHANGE_VIEWMODE();
 
 	/* <---STATIONARY MODE---> */
 	if(mode == 0x00){
@@ -402,11 +486,16 @@ void SET_MODE(){
 }
 
 void SET_WARNING(){
-	if(temp_warning_flag == 1){
+	if(mode == 0x02){
+		ACCELEROMETER();
+	}
+
+	if(temp_warning_flag == 1 && temp_warning_message_flag == 0){
 		stateStrPtr = "Temp. too high";
 		if(acc_warning_flag == 1){
 			if(temp_warning_message_flag == 0){
 				//print oled temp warning message under acc warning message
+				oled_putString(0,28, (unsigned char*)stateStrPtr, OLED_COLOR_WHITE,OLED_COLOR_BLACK);
 				temp_warning_message_flag = 1;
 			}
 		} else {
@@ -430,12 +519,19 @@ void SET_WARNING(){
 	else if(acc_warning_flag == 1){
 		stateStrPtr = "Veer off course";
 		if(temp_warning_flag == 1){
-			//display acc warning message under already displayed temp warning message
+			if(acc_warning_message_flag == 0){
+				//display acc warning message under already displayed temp warning message
+				oled_putString(0,28, (unsigned char*)stateStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+				acc_warning_message_flag = 1;
+			}
 		} else {
-			oled_putString(0, 20, (unsigned char*)stateStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+			if(acc_warning_message_flag == 0){
+				oled_clearScreen(OLED_COLOR_BLACK);
+				oled_putString(0, 20, (unsigned char*)stateStrPtr, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+				acc_warning_message_flag = 1;
+			}
 		}
 		check_clearWarning();
-		blink_blue();
 	}
 }
 
@@ -447,7 +543,9 @@ int main (void) {
     init_i2c();
     init_ssp();
 	init_Timer0();
-	init_Timer2();	//init timer 2 for temperature interrupt
+	init_Timer1();
+	init_Timer2();	//init timer 2
+	init_Timer3();
 
 	pca9532_init(); //led_array
 	acc_init();
@@ -455,24 +553,46 @@ int main (void) {
     oled_init();
     led7seg_init();
 
+    //Clears all interrupts
+    NVIC_ClearPendingIRQ(EINT0_IRQn);
 	NVIC_ClearPendingIRQ(EINT3_IRQn);
-	LPC_GPIOINT->IO2IntEnR |= 1<<10;
-	LPC_GPIOINT->IO0IntEnF |= 1<<2;
-	NVIC_EnableIRQ(EINT3_IRQn);
+	NVIC_ClearPendingIRQ(TIMER1_IRQn);
+	NVIC_ClearPendingIRQ(TIMER3_IRQn);
 
+	//Set up EINT0 for SW3
+	LPC_SC->EXTMODE |= 1<<0;
+	LPC_SC->EXTPOLAR = 0;
+
+	//Set up temp sensor interrupt
+	LPC_GPIOINT->IO0IntEnF |= 1<<2;
+
+	//Set up interrupt priorities
+	NVIC_SetPriorityGrouping(5);
+	NVIC_SetPriority(SysTick_IRQn,0x00);
+	NVIC_SetPriority(EINT0_IRQn,0x40);
+	NVIC_SetPriority(EINT3_IRQn,0x48);
+	NVIC_SetPriority(TIMER1_IRQn,0x50);
+	NVIC_SetPriority(TIMER3_IRQn,0x60);
+
+	NVIC_EnableIRQ(EINT0_IRQn);
+	NVIC_EnableIRQ(EINT3_IRQn);
+	NVIC_EnableIRQ(TIMER1_IRQn);
+	NVIC_EnableIRQ(TIMER3_IRQn);
+
+	toggle_count = 0; //init number of sw3 button presses to be 0
 	sw4btn = 1; //init sw4 button
 	mode = 0; //init as STATIONARY MODE
 
-	//set initial OLED and SSEG displays
+	//Set initial OLED and SSEG displays
 	oled_clearScreen(OLED_COLOR_BLACK);
 	led7seg_setChar(sseg_chars[stationary_counter], TRUE);
 
-	//get initial offset for accelerometer
+	//Get initial offset for accelerometer
 	acc_read(&x, &y, &z);
 	xoff = (0-x)/1.0;
 	yoff = (0-y)/1.0;
 
-	//reset flag statuses
+	//Reset flag statuses
 	acc_warning_flag = 0;
 	acc_warning_message_flag = 0;
 	temp_warning_flag = 0;
